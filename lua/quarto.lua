498619ebd08c0b661b61ed8f14990a5cecbbdf3b
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-05-20 23:18:31 +0300

    add quartoPreview()
    which automatically detects a project vs. a single file.

--- lua/quarto.lua
+++ lua/quarto.lua
@@ -1,37 +1,44 @@
 local M = {}
 local api = vim.api
+local util = require("lspconfig.util")
 
-function M.previewFile()
-  local filepath = api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) or ''
-  local filename = filepath:match("^.+/(.+)$")
-  local command = 'quarto preview ' .. filename
-  vim.cmd('tabedit term://' .. command)
-end
-
-
-function M.renderFile()
-  local filepath = api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) or ''
-  local filename = filepath:match("^.+/(.+)$")
-  local command = 'quarto render ' .. filename
-  vim.cmd('tabedit term://' .. command)
-end
-
-
-function M.previewProject()
-  local command = 'quarto preview'
-  vim.cmd('tabedit term://' .. command)
-end
-
-function M.renderProject()
-  local command = 'quarto render'
-  vim.cmd('tabedit term://' .. command)
-end
-
-function M.setup(opts)
-  opts = opts or {}
-  if opts.debug then
-    print("quarto-nvim set up.")
+local function contains(list, x)
+  for _, v in pairs(list) do
+    if v == x then return true end
   end
+  return false
 end
+
+function M.quartoPreview()
+  -- Modelled after quarto-emacs:
+  -- <https://github.com/quarto-dev/quarto-emacs/blob/main/quarto-mode.el>
+
+  -- find root directory / check if it is a project
+  local buffer_path = vim.api.nvim_buf_get_name(0)
+  local root_dir = util.root_pattern("_quarto.yml")(buffer_path)
+  local command
+  local mode
+  if root_dir then
+    mode = "project"
+    command = 'quarto preview'
+  else
+    mode = "file"
+    command = 'quarto preview ' .. buffer_path
+  end
+
+  local quarto_extensions = { ".qmd", ".Rmd", ".ipynb", ".md" }
+  local file_extension = buffer_path:match("^.+(%..+)$")
+  if mode == "file" and not contains(quarto_extensions, file_extension) then
+    vim.notify("Not a quarto file, ends in " .. file_extension .. " exiting.")
+    return
+  end
+
+  -- run command in embedded terminal
+  -- in a new tab and go back to the buffer
+  vim.cmd('tabedit term://' .. command)
+  vim.cmd('tabprevious')
+
+end
+
 
 return M
