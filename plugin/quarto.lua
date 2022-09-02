ace305e6c7e56ecab19122233398b7d414c62a62
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-09-02 09:02:02 +0200

    disable autocommand

--- plugin/quarto.lua
+++ plugin/quarto.lua
@@ -3,13 +3,13 @@ local a = vim.api
 a.nvim_create_user_command('QuartoPreview', require'quarto'.quartoPreview, {})
 a.nvim_create_user_command('QuartoDiagnostics', require'quarto'.enableDiagnostics, {})
 
-a.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = '*.qmd',
-  group = a.nvim_create_augroup('quarto', {}),
-  callback = function(args)
-    -- use markdown ft until quarto ft is more widespread
-    a.nvim_buf_set_option(0,'filetype', 'markdown')
-    require'quarto'.enableDiagnostics()
-  end
-})
+-- a.nvim_create_autocmd({ "BufEnter" }, {
+--   pattern = '*.qmd',
+--   group = a.nvim_create_augroup('quarto', {}),
+--   callback = function(args)
+--     -- use markdown ft until quarto ft is more widespread
+--     a.nvim_buf_set_option(0,'filetype', 'markdown')
+--     require'quarto'.enableDiagnostics()
+--   end
+-- })
 
