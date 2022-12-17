0ae0477f7511b03655c1ebb1974db3cfe2e78c8d
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-12-17 19:21:48 +0100

    remove debug setup function

--- plugin/quarto.lua
+++ plugin/quarto.lua
@@ -1,14 +1,6 @@
 local quarto = require'quarto'
 local a = vim.api
 
--- debug
-quarto.setup{
-  lspFeatures = {
-    enabled = true,
-    languages = { 'python' }
-  }
-}
-
 a.nvim_create_user_command('QuartoPreview', quarto.quartoPreview, {})
 a.nvim_create_user_command('QuartoClosePreview', quarto.quartoClosePreview, {})
 a.nvim_create_user_command('QuartoDiagnostics', quarto.enableDiagnostics, {})
