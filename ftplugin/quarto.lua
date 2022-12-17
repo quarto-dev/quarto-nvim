ac175a2a09b9b42d1b12bff759ae87fdde49ee04
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-12-17 19:17:36 +0100

    add ftplugin to use markdown ft for quarto

---
+++ ftplugin/quarto.lua
@@ -0,0 +1,5 @@
+-- set filetype to markdown for now,
+-- until we have our own e.g. treesitter grammar
+vim.bo.filetype = 'markdown'
+
+vim.b.slime_cell_delimiter = "```"
--- plugin/quarto.lua
+++ plugin/quarto.lua
@@ -14,14 +14,3 @@ a.nvim_create_user_command('QuartoClosePreview', quarto.quartoClosePreview, {})
 a.nvim_create_user_command('QuartoDiagnostics', quarto.enableDiagnostics, {})
 a.nvim_create_user_command('QuartoHelp', quarto.searchHelp, {nargs=1})
 
-
--- a.nvim_create_autocmd({ "BufEnter" }, {
---   pattern = '*.qmd',
---   group = a.nvim_create_augroup('quarto', {}),
---   callback = function(args)
---     -- use markdown ft until quarto ft is more widespread
---     a.nvim_buf_set_option(0,'filetype', 'markdown')
---     require'quarto'.enableDiagnostics()
---   end
--- })
-
