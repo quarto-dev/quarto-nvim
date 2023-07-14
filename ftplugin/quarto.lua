0c6bf2677cdd11995c67d1b5a864cb5e9b09387b
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-07-14 11:47:38 +0200

    !feat: move keymap setup to autocommand instead of ftplugin
    and creat autocommand for lspattach to prevent other
    language servers from overwriting quarto-nvim keybindings

--- ftplugin/quarto.lua
+++ ftplugin/quarto.lua
@@ -1,14 +1,2 @@
 vim.b.slime_cell_delimiter = "```"
 
-local q = require'quarto'
-if q.config.lspFeatures.enabled and vim.bo.buftype ~= 'terminal' then
-  q.activate()
-
-  vim.api.nvim_buf_set_keymap(0, 'n', q.config.keymap.definition, ":lua require'otter'.ask_definition()<cr>", { silent = true })
-  vim.api.nvim_buf_set_keymap(0, 'n', q.config.keymap.type_definition, ":lua require'otter'.ask_type_definition()<cr>", { silent = true })
-  vim.api.nvim_buf_set_keymap(0, 'n', q.config.keymap.hover, ":lua require'otter'.ask_hover()<cr>", { silent = true })
-  vim.api.nvim_buf_set_keymap(0, 'n', q.config.keymap.rename, ":lua require'otter'.ask_rename()<cr>", { silent = true })
-  vim.api.nvim_buf_set_keymap(0, 'n', q.config.keymap.references, ":lua require'otter'.ask_references()<cr>", { silent = true })
-  vim.api.nvim_buf_set_keymap(0, 'n', q.config.keymap.document_symbols, ":lua require'otter'.ask_document_symbols()<cr>", { silent = true })
-end
-
