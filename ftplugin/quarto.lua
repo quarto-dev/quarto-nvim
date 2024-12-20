c249669a8e6312ef5b1c5a19e484b712a0680f28
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-12-20 17:37:42 +0100

    dev for pandoc markdown treesitter grammar

--- ftplugin/quarto.lua
+++ ftplugin/quarto.lua
@@ -2,7 +2,9 @@ vim.b.slime_cell_delimiter = '```'
 
 -- TODO: Workaround while nvim-treesitter doesn't link those anymore
 -- until our ouwn pandoc grammar is ready
-vim.treesitter.language.register("markdown", { "quarto", "rmd" })
+-- vim.treesitter.language.register("markdown", { "quarto", "rmd" })
+
+vim.bo.commentstring = "<!-- %s -->"
 
 local config = require('quarto.config').config
 local quarto = require 'quarto'
