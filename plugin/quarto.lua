fb3f124b6bf73976ecf9595f41e472f3e2b0a9ea
Jannik Buhr <jannik.m.buhr@gmail.com>
2026-01-16 15:10:48 +0100

    fix: make QuartoConfig allways available

--- plugin/quarto.lua
+++ plugin/quarto.lua
@@ -1,3 +1,5 @@
+require 'quarto.config'
+
 if vim.fn.has 'nvim-0.9.0' ~= 1 then
   local msg = [[
   quarto-dev/quarto-nvim and jmbuhr/otter.nvim require Neovim version >= 0.9.0 (https://github.com/neovim/neovim/releases/tag/stable).
